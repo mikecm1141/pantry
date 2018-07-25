f6b9b84455a1c4d6cf3ff6593ca5caef45259486
Mike McKee <mikecm@gmail.com>
2018-07-25 14:22:24 -0600

    Finishing up tests and implementation, some refactoring

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -36,6 +36,34 @@ class Pantry
   end
 
   def what_can_i_make
-  
+    able_to_make = []
+    @cookbook.each do |recipe|
+      valid_recipe = recipe.ingredient_types.all? do |ingredient|
+        recipe.amount_required(ingredient) <= @stock[ingredient]
+      end
+      able_to_make << recipe.name if valid_recipe
+    end
+    able_to_make
+  end
+
+  def find_recipes_by_name(recipes)
+    @cookbook.find_all do |recipe|
+      recipes.member?(recipe.name)
+    end
+  end
+
+  def how_many_can_i_make
+    recipe_names     = what_can_i_make
+    recipe_objects   = find_recipes_by_name(recipe_names)
+    amount_available = Hash.new(0)
+
+    recipe_objects.each do |recipe|
+      amount = recipe.ingredient_types.map do |ingredient|
+        @stock[ingredient] / recipe.amount_required(ingredient)
+      end
+      amount_available[recipe.name] = amount.min
+    end
+    amount_available
+    require "pry"; binding.pry
   end
 end
